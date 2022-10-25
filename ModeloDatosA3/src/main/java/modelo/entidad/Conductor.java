21697c9034027355d402f1827481b4c3befe36e0
Alberto Sandino Hernández <albertosandinohdez@gmail.com>
2022-10-25 23:39:33 +0200

    Initial commit

---
+++ ModeloDatosA3/src/main/java/modelo/entidad/Conductor.java
@@ -0,0 +1,30 @@
+package modelo.entidad;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="conductores")
+public class Conductor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	String nombre;
+	String dni;
+	
+	@Temporal(TemporalType.DATE)
+	private Date fechaNacimiento;
+	
+	@ManyToMany
+	@JoinTable(name="conductores_coche",
+			joinColumns= {@JoinColumn(name= "fk_id_conductor", referencedColumnName="id")},
+			inverseJoinColumns= {@JoinColumn(name="fk_id_coche", referencedColumnName ="id")})
+	private List<Coche> coches;
+	
+	
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java Maven Data Model
- A Java program that lets you manage car information through Apache's Maven.
+A Java program that lets you manage a SQL database full of car information through Apache's Maven.
+
+Un programa Java que te permite manejar una base de datos SQL con información de coches a través de Maven, de Apache. 
